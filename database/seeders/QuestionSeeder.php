d8f4108a012cdac27327fdbadceeaabf5dc4362c
Taha Çiçek <ttahacicek@gmail.com>
2022-09-03 11:37:03 +0300

    question table migration, seed, factory created.

---
+++ database/seeders/QuestionSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class QuestionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\Question::factory(100)->create();
+
+    }
+}
